3a77d92822d254fb355ae64e96f535f599030ff4
Evgeny Kaminsky <evgeny.kaminsky@gmail.com>
2023-07-12 18:16:08 -0400

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Evgeny Kaminsky on 2023-07-12.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
